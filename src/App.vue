eb5d8131f3ad4afead5405585e4c23a35c2c54bf
sisdream <sissipascale2@gmail.com>
2024-02-19 12:47:24 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import { store } from "./components/store.js";
+
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+
+
+export default {
+  data(){
+    return{
+      store,
+    }
+  },
+  components: { AppHeader, AppMain, AppFooter},
+}
+
+</script>
+
+<template>
+  <AppHeader />
+
+  <AppMain />
+
+  <AppFooter />
+
+</template>
+
+<style lang="scss">
+@use './styles/genetal.scss'
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<template>
+ <header>
+    <section class="banda">
+        <h1>titolo</h1>
+        
+    </section>
+ </header>
+</template>
+
+<style lang="scss">
+.banda{
+    height: 30px;
+    background-color: #3b3b3b;
+}
+</style>
