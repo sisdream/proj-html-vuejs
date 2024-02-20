706a8cf34cd862a7e6952934c1a42ca50638ba88
sisdream <sissipascale2@gmail.com>
2024-02-20 12:27:30 +0100

    feat: section info

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,18 +1,18 @@
 <script>
 export default {
-        data () {
-            return {
-                links: [
-                    'Careers',
-                    'News',
-                    'Terms of use',
-                    'Privacy Projects',
-                    'About',
-                    'Contact'
-                ],
-            }
-        },
-    }
+    data () {
+        return {
+            links: [
+                'Careers',
+                'News',
+                'Terms of use',
+                'Privacy Projects',
+                'About',
+                'Contact'
+            ],
+        }
+    },
+}
 </script>
 
 <template>
@@ -23,7 +23,7 @@ export default {
                 <div class="logo">
                     <img src="/images/cropped-Group-39-2x.png" alt="logo">
                 </div>
-                <p>
+                <p class="mt-3">
                     While the lovely valley teems whit vapour around me, 
                     and the meridian sun strikes the upper surface of the 
                     impenetrable foliage of my trees.while the lovely valley
@@ -32,15 +32,29 @@ export default {
             </div>
             <div class="col">
                 <h4>QUICK LINKS</h4>
-                <ul class="d-block ">
+                <ul class="p-0 d-flex flex-column">
                     <li v-for="link in links">
                         <a href="#">{{ link }}</a>
                     </li>
                 </ul>
-
             </div>
-            <div class="col"></div>
-            <div class="col"></div>
+            <div class="col">
+                <h4>CONTACT</h4>
+                <ul class="p-0 d-flex flex-column">
+                    <li>
+                        Patricia C. Amedee 4401 Waldeck Street
+                        Grapevine Nashville, TX 76051
+                    </li>
+                    <li><a href="#">info@yourdomain.com</a></li>
+                    <li>+99 (0) 101 0000 888</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h4>LOCATION ON MAP</h4>
+                <div class="map">
+                    <img src="/images/map.png" alt="map">
+                </div>
+            </div>
         </div>
     </section>
     <section class="copy"></section>
@@ -49,6 +63,12 @@ export default {
 
 <style>
 .info{
-    height: 700px;
+    padding: 100px;
+}
+
+.copy{
+    background: rgb(2,0,36);
+    background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(115,115,115,1) 0%, rgba(24,24,24,1) 27%); 
+    height: 100px;
 }
 </style>
